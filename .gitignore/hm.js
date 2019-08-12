290b973cfdf12e282cbedf246fa51c0b594ae90c
MucusSorevegnty <39030321+MucusSorevegnty@users.noreply.github.com>
2019-08-12 11:56:37 +0200

    Update hm.js

--- .gitignore/hm.js
+++ .gitignore/hm.js
@@ -8,16 +8,11 @@ client.once('ready', () => {
 
 
 client.on('message', message => {
-try {
-   
+
 if(message.content.startsWith('$work')){
   if(message.author.id != client.user.id){ return }
   var Rdm = Math.floor(Math.random() * (400000 - 300000)) + 300000; 
   setTimeout(() => message.channel.send('$work'), (Rdm))
-}
-
-} catch (error) {
-   console.log(error)
 }
 
 });
